abd14ccfadb8fda1756d142875a05cb39d8f82a0
Tejaswi3579 <157278900+Tejaswi3579@users.noreply.github.com>
2026-03-17 18:17:24 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,18 +11,18 @@ st.title("🏦 Bank Customer Churn Analytics")
 @st.cache_resource
 def get_connection():
     conn = snowflake.connector.connect(
-        user=os.getenv("SNOWFLAKE_USER"),
-        password=os.getenv("SNOWFLAKE_PASSWORD"),
-        account=os.getenv("SNOWFLAKE_ACCOUNT"),
-        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
-        database=os.getenv("SNOWFLAKE_DATABASE"),
-        schema=os.getenv("SNOWFLAKE_SCHEMA"),
-        role=os.getenv("SNOWFLAKE_ROLE")
+        user=st.secrets["snowflake"]["user"],
+        password=st.secrets["snowflake"]["password"],
+        account=st.secrets["snowflake"]["account"],
+        warehouse=st.secrets["snowflake"]["warehouse"],
+        database=st.secrets["snowflake"]["database"],
+        schema=st.secrets["snowflake"]["schema"],
+        role=st.secrets["snowflake"]["role"]
     )
     return conn
 
 conn = get_connection()
-st.success("✅ Connected to Snowflake")
+st.success("✅ Connected to Snowflake Successfully!")
 
 # ---------------- LOAD DATA ----------------
 @st.cache_data
